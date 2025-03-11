2728f8e860af3d050df83ddb2759aa0e2b294fbf
sumit <sumitronaldo111@gmail.com>
2025-03-12 02:40:50 +0530

    asd

---
+++ app_ssh.py
@@ -0,0 +1,25 @@
+from fabric import Connection, Config
+import getpass
+import re
+password = getpass.getpass("Enter your root password: ")
+configure = Config(overrides={'sudo': {'password': password}})
+conn = Connection(host="13.232.236.118", user="ubuntu", connect_kwargs={"password": password}, config=configure)
+
+#conn.run("sudo su -")
+conn.sudo("apt update -y")
+conn.sudo("apt install python3")
+#conn.sudo("apt install net-tools")
+ip_address = "13.232.236.118"
+
+desktop_path = "~/"
+
+with conn.cd(desktop_path):
+    conn.run("rm -rf test_folder")
+    conn.run("mkdir test_folder")
+    with conn.cd(desktop_path+"/test_folder"):
+        conn.run("rm -rf *")
+        conn.run('echo "Hello World From inside\
+            the Ubuntu Terminal" > hello.txt')
+        conn.run("cat hello.txt")
+        conn.run(f'echo "Your IP Address is: {ip_address}" > ip.txt')
+        conn.run("cat ip.txt")
